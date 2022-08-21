751c8354f7b524accd9e69501361c55677e62558
GRIN-96 <108512496+GRIN-96@users.noreply.github.com>
2022-08-22 00:59:36 +0900

    [Bronze V] Title: 학점계산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+a = input()
+
+if a == 'A+' :
+    print(4.3)
+if a == 'A0' :
+    print(4.0)
+if a == 'A-' :
+    print(3.7)
+if a == 'B+' :
+    print(3.3)
+if a == 'B0' :
+    print(3.0)
+if a == 'B-' :
+    print(2.7)
+if a == 'C+' :
+    print(2.3)
+if a == 'C0' :
+    print(2.0)
+if a == 'C-' :
+    print(1.7)
+if a == 'D+' :
+    print(1.3)
+if a == 'D0' :
+    print(1.0)
+if a == 'D-' :
+    print(0.7)
+if a == 'F' :
+    print(0.0)
