40fea16175c9ddbcc42c22cc1db87b637658d07f
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2022-10-07 17:50:55 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,14 @@
+let input = readLine()!
+let a = Int(input)!
+
+if 90 <= a {
+    print("A")
+}else if 80 <= a{
+    print("B")
+}else if 70 <= a{
+    print("C")
+}else if 60 <= a{
+    print("D")
+}else {
+    print("F")
+}
