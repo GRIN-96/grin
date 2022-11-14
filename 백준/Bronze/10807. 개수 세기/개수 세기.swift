7375485ec2626b55d0c54eefbde3f412fadc1644
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2022-11-14 23:12:29 +0900

    [Bronze V] Title: 개수 세기, Time: 16 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let count = readLine()!
+let nums = readLine()!.components(separatedBy: " ").map({String($0)})
+let v = readLine()!
+var result = 0
+
+for i in nums {
+    if String(i) == v { result += 1 }
+}
+
+print(result)
