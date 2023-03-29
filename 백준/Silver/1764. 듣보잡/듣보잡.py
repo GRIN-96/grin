45a901b42d43d5b06681ba76abb27db5c06f878d
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2023-03-29 21:16:03 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 43908 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr1 = []
+arr2 = []
+for i in range(n):
+    x = input()
+    arr1.append(x)
+for i in range(m):
+    y = input()
+    arr2.append(y)
+
+answer = list(set(arr1) & set(arr2))
+answer.sort()
+print(len(answer))
+print(''.join(answer), end = '')
