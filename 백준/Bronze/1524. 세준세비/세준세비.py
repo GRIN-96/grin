ddc4f82bda8b7ebcfde785ba89b17f10fd922c7f
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2023-06-19 20:21:31 +0900

    [Bronze I] Title: 세준세비, Time: 252 ms, Memory: 40416 KB -BaekjoonHub

---
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -0,0 +1,19 @@
+t = int(input())
+for i in range(t) :
+    input()	
+    N, M = map(int, input().split())
+    sj = sorted(list(map(int, input().split())), reverse=True)
+    sb = sorted(list(map(int, input().split())), reverse=True)
+    
+    while sj and sb :	
+        if sj[-1] >= sb[-1] :	
+            sb.pop()
+        else :
+            sj.pop()
+    
+    if sj :
+        print('S')
+    elif sb :
+        print('B')
+    else :
+        print('C')
