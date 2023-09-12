cb7d6215438896f548a2a4b23ab7a541845ea640
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2023-09-12 20:20:06 +0900

    [Bronze II] Title: 분산처리, Time: 1048 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
-
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
 public class Main {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int T = s.nextInt();
-		int a,b,r;
-		for(int j=0;j<T;j++) {
-			a=s.nextInt();
-			b=s.nextInt();
-			r=1;
-			
-			for (int i=0;i<b;i++) r=(r*a)%10;
-			if(r==0) r=10;
-			System.out.println(r);
-		}
-		s.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a % 10 == 0) {
+                sb.append(10 + "\n");
+                continue;
+            }
+            int num = a % 10;
+            for (int j = 1; j < b; j++) {
+                num = (num * a) % 10;
+            }
+            sb.append(num + "\n");
+        }
+        System.out.print(sb);
+    }
 }
