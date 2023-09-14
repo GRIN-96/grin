10be1fd75ae4bd120becc724122f9f008c501c6e
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2023-09-14 22:05:14 +0900

    [unrated] Title: 특수문자 출력하기, Time: 140.04 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        String ans = "!@#$%^&*(\\'\"<>?:;";
+        
+        System.out.println(ans);
+    }
+}
