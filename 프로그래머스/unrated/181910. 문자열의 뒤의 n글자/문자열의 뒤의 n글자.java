b3420f64faf04f2bd6cc5a1501be6fc44e19aaea
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2023-11-27 12:32:01 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 1.26 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        int num = my_string.length() - n;
+            
+        for (int i = num; i < my_string.length(); i++ ) {
+            answer += Character.toString(my_string.charAt(i));
+        }
+        
+        return answer;
+    }
+}
