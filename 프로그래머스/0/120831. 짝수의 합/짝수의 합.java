3992e18ff91e29592f687cba1cda37b4d40af183
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-04-01 21:28:36 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i=1; i<n+1; i++) {
+            answer += (i % 2 == 0) ? i : 0; 
+        }
+        return answer;
+    }
+}
