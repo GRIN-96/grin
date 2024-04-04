cf65501481963ee9a723bc8091d000f711ffa28a
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-04-05 07:22:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer str = new StringBuffer(my_string);
+        
+        return str.reverse().toString();
+    }
+}
