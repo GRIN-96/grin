ec41bd6455f8f56662981b2fcdce855537be121c
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-04-08 07:28:25 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+
+        for(int i = 0; i < num_list.length; i++) {
+            answer[num_list.length - i - 1] = num_list[i];
+        }
+
+        return answer;
+    }
+}
