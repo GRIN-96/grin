067cd1ddfa902a6d915edefe35ed852b36cd6d83
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-04-30 14:36:02 +0900

    [level 0] Title: 가위 바위 보, Time: 10.55 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        char scissors = '2';
+        char rock = '0';
+        char paper = '5';
+        
+        for(int i = 0; i < rsp.length(); i++){
+            if(rsp.charAt(i) == scissors){
+                
+                answer += rock;
+                
+            }else if(rsp.charAt(i) == rock){
+            
+                answer += paper;
+                
+            }else if(rsp.charAt(i) == paper){
+                 
+                answer += scissors;
+            
+            }
+        }
+            
+        return answer;
+    }
+}
