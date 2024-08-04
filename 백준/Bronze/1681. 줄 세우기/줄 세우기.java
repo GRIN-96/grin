aa59494d777dd1b3ee05d58a5917429296f2b80f
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-08-05 07:27:07 +0900

    [Bronze II] Title: 줄 세우기, Time: 236 ms, Memory: 62380 KB -BaekjoonHub

---
+++ 백준/Bronze/1681. 줄 세우기/줄 세우기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static String NOT_INCLUDED_NUM;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int studentNum = Integer.parseInt(st.nextToken());
+        NOT_INCLUDED_NUM = st.nextToken();
+
+        int count = 0;
+        while (studentNum>0){
+            count++;
+            if (!String.valueOf(count).contains(NOT_INCLUDED_NUM)){
+                studentNum--;
+            }
+        }
+
+        System.out.print(count);
+    }
+
+}
