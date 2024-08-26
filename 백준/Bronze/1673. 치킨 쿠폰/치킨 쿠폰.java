d790753f0f9960647e285403a383da95ff002e63
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-08-27 07:34:39 +0900

    [Bronze II] Title: 치킨 쿠폰, Time: 188 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Bronze/1673. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int temp = n, add_coupon = 0;
+            while(temp >= k){
+                add_coupon += temp / k;
+                temp = temp/k + temp%k;
+            }
+            System.out.println(n+add_coupon);    
+        }
+    }
+}
