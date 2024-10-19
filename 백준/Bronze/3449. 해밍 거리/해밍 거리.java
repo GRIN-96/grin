53464be25505334ab4eefe96223fa3a6c2761a2b
Grin_:) <108512496+GRIN-96@users.noreply.github.com>
2024-10-19 18:44:19 +0900

    [Bronze III] Title: 해밍 거리, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/3449. 해밍 거리/해밍 거리.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String a = br.readLine();
+            String b = br.readLine();
+            int distance = 0;
+
+            for (int j = 0; j < a.length(); j++) {
+                if (a.charAt(j) != b.charAt(j)) {
+                    distance++;
+                }
+            }
+
+            bw.write("Hamming distance is " + distance + ".\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
